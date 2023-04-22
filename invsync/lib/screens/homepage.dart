642c75d1afb573b266bd355116dbdccfe402fedb
Omjannu <89771105+Om-jannu@users.noreply.github.com>
2023-04-23 02:36:30 +0530

    added inventory page

--- invsync/lib/screens/homepage.dart
+++ invsync/lib/screens/homepage.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:invsync/widgets/myDrawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Title(color: Colors.black, child: const Text("InvSync")),
+    //   ),
+    //   drawer: MyDrawer(),
+    // );
     return Scaffold(
       appBar: AppBar(
-        title: Title(color: Colors.black, child: const Text("InvSync")),
+        title: Text('My App'),
+      ),
+      drawer: MyDrawer(),
+      body: Center(
+        child: Text('This is the inventory screen'),
       ),
     );
   }
