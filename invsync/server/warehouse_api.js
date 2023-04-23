efc1ff250687b25b72732d7c623a2c7b63ba9353
PurpleVen <vendra0408@gmail.com>
2023-04-23 17:50:08 +0530

    change in server

--- invsync/server/warehouse_api.js
+++ invsync/server/warehouse_api.js
@@ -12,51 +12,52 @@ const config = {
   appId: '1:478966296027:android:4787a9ad6fd64f05e28bfe'
 };
 
+
 // initialize it 
 
 firebase.initializeApp(config);
 const db = firebase.database();
 
-//set up backend and port
+// set up backend and port
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-//generate random values for the temp, humidity and co2 along with timestamp
+// generate random values for the temp, humidity and co2 along with timestamp
 
 function generateRandomValue(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-// use api with get request to create a record
+// use api with get request to create records with 5 minute difference
 
 app.get('/addData', (req, res) => {
-  const temperature = generateRandomValue(10, 45);
-  const humidity = generateRandomValue(40, 90);
-  const co2 = generateRandomValue(300, 1000);
-
-//   const timestamp = new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
-
-const date = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
-const timestamp = new Date(date).toISOString();
-console.log(timestamp); // output: 2023-04-22T19:30:12.000Z
-
-
-  // Append the generated data to the Firebase database
-  db.ref('sensorData').push({
-    temperature: temperature,
-    humidity: humidity,
-    co2: co2,
-    timestamp: timestamp
-  }, (error) => {
-    if (error) {
-      res.status(500).send('Error adding data to database');
-    } else {
-      res.send('Data added to database');
-    }
-  });
+  const startTime = new Date().getTime() - 43200000; // 12 hours in milliseconds
+  const endTime = new Date().getTime();
+  const timeDifference = 300000; // 5 minutes in milliseconds
+  let timestamp = new Date(startTime);
+  while (timestamp.getTime() < endTime) {
+    const temperature = generateRandomValue(10, 45);
+    const humidity = generateRandomValue(40, 90);
+    const co2 = generateRandomValue(300, 1000);
+    const newRecordRef = db.ref('sensorData').push({
+      temperature: temperature,
+      humidity: humidity,
+      co2: co2,
+      timestamp: timestamp.toISOString()
+    }, (error) => {
+      if (error) {
+        console.log('Error adding data to database');
+      } else {
+        console.log('Data added to database with ID:', newRecordRef.key);
+      }
+    });
+    timestamp = new Date(timestamp.getTime() + timeDifference);
+  }
+  res.send('Data added to database');
 });
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
